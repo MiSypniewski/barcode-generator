e8cf7780a5482ea0d4bfe086d82a97d350a1452d
MiSypniewski <mi.sypniewski@gmail.com>
2020-04-13 11:38:36 +0200

    Updates

---
+++ precache-manifest.aac6098b82e6067a9ca4f4c76b3f9979.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f701785e2b92aff1bfb0a970bcaba0b",
+    "url": "/barcode-generator/index.html"
+  },
+  {
+    "revision": "5ce0290168bd5ffb9522",
+    "url": "/barcode-generator/static/js/2.a2574602.chunk.js"
+  },
+  {
+    "revision": "01898b9da533433ae3c9",
+    "url": "/barcode-generator/static/js/main.040a1b72.chunk.js"
+  },
+  {
+    "revision": "e1e7a6502966da8ad061",
+    "url": "/barcode-generator/static/js/runtime~main.f5da3db4.js"
+  }
+]);
